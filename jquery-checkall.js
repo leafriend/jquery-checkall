efb38997bb7fcd90d791b9366cd833a8ffcca86d
leafriend <leafriend@gmail.com>
2014-10-12 15:00:42 +0900

    Implement check all

--- jquery-checkall.js
+++ jquery-checkall.js
@@ -1,5 +1,24 @@
 
 (function($) {
-	$.fn.checkAll = function() {
-	};
+    $.fn.checkAll = function() {
+
+        var args;
+        if (arguments.length == 0) {
+            args = ["input[type=checkbox]"];
+        } else {
+            args = arguments;
+        }
+
+        return this.each(function() {
+
+            $(this).click(function () {
+                var checked = $(this).prop("checked");
+                for (var i = 0; i < args.length; i++) {
+                    $(args[i]).prop("checked", checked);
+                }
+            });
+
+        });
+
+    };
 }(jQuery));
